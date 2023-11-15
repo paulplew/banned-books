657101572e61d68e3319afb82e38496baff7dbb1
paulplew <thepaulplew@gmail.com>
2023-11-14 20:16:05 -0500

    Make the routes serve.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiohttp import web
 from backend import run
 
 app = web.Application()
-app.add_routes([web.get("/", run), web.get("/{ascii_width}", run)])
+app.add_routes([web.get("/{ascii_width}", run)])
 
 if __name__ == "__main__":
     web.run_app(app)
